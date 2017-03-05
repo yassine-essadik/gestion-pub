0bfc6d1fecd466bd978901cd28b8da8b7cff6ada
Yassine Essadik <essadik34@gmail.com>
2017-03-05 22:04:13 +0000

    Front

--- src/GP/FrontBundle/Form/RegisterType.php
+++ src/GP/FrontBundle/Form/RegisterType.php
@@ -1,48 +1,48 @@
 <?php
 
-namespace GP\FrontBundle\Form\Type;
+namespace GP\FrontBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use FOS\UserBundle\Form\Type\RegistrationFormType;
 
-class RegisterType extends RegistrationFormType
+class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
 
         $builder
-            ->add('gender', 'text', array(
+            ->add('gender', TextType::class, array(
                 'label' => 'profile.fields.gender',
                 'translation_domain' => 'forms'
             ))
-            ->add('firstname', 'text', array(
+            ->add('firstname', TextType::class, array(
                 'label' => 'profile.fields.firstname',
                 'translation_domain' => 'forms'
             ))
-            ->add('lastname', 'text', array(
+            ->add('lastname', TextType::class, array(
                 'label' => 'profile.fields.lastname',
                 'translation_domain' => 'forms'
             ))
-            ->add('address', 'text', array(
+            ->add('address', TextType::class, array(
                 'label' => 'profile.fields.address',
                 'translation_domain' => 'forms'
             ))
-            ->add('zip_code', 'text', array(
+            ->add('zip_code', TextType::class, array(
                 'label' => 'profile.fields.zip_code',
                 'translation_domain' => 'forms'
             ))
-            ->add('city', 'text', array(
+            ->add('city', TextType::class, array(
                 'label' => 'profile.fields.city',
                 'translation_domain' => 'forms'
             ))
-            ->add('country', 'text', array(
+            ->add('country', TextType::class, array(
                 'label' => 'profile.fields.country',
                 'translation_domain' => 'forms'
             ))
-            ->add('phone', 'text', array(
+            ->add('phone', TextType::class, array(
                 'label' => 'profile.fields.phone',
                 'translation_domain' => 'forms'
             ))
@@ -56,8 +56,8 @@ class RegisterType extends RegistrationFormType
         ));
     }
 
-    public function getName()
+    public function getParent()
     {
-        return 'gp_fos_user_register';
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
     }
 }
