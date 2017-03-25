de47b75d2044841fc67f9bf460f14347c298bf65
Yassine Essadik <essadik34@gmail.com>
2017-03-25 18:20:09 +0000

    Form

--- src/GP/GestionBundle/Controller/ParametrageController.php
+++ src/GP/GestionBundle/Controller/ParametrageController.php
@@ -25,21 +25,37 @@ class ParametrageController extends Controller
     	
     }
     
-    public function societeAction($id, Request $request)
+    public function societeAction($id=null, Request $request)
     {
-    	$societe = new Societe();
-    	$form   = $this->get('form.factory')->create(SocieteType::class, $societe);
+    	$em = $this->getDoctrine()->getManager();
     	
+    	if(!empty($id))
+    	{
+    		$societe = $em->getRepository('GPGestionBundle:Societe')->find($id);
+    		if (empty($societe)) 
+    		{
+    			throw new NotFoundHttpException("L'enregistrement d'id ".$id." n'existe pas.");
+    		}
+    	}
+    	else
+    	{
+    		$societe = new Societe();
+    	}
+    	
+    	$form   = $this->get('form.factory')->create(SocieteType::class, $societe);
+
     	if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-    		$em = $this->getDoctrine()->getManager();
+    		
     		$em->persist($societe);
     		$em->flush();
-    	
-    		$request->getSession()->getFlashBag()->add('notice', 'Item bien enregistré.');
+    		$request->getSession()->getFlashBag()->clear();
+    		$request->getSession()->getFlashBag()->add('notice', 'Elément bien enregistré.');
+
+    		return $this->redirectToRoute('gb_gestion_bundle_parametrage_societes_edit', array('id' => $societe->getId()));
     	}
     	
     	return $this->render('GPGestionBundle:Parametrage:societe.html.twig', array(
-    			'form' => $form->createView(),
+    			'form' => $form->createView(), 'item' => $societe
     	));
     	 
     }
